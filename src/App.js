cdb411813824c8f83f29cf8f66d2d96caad72fa5
ayushi-2607 <46553328+ayushi-2607@users.noreply.github.com>
2020-12-07 20:59:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component } from "react";
+import Login from './screens/login';
+// import ReactDOM from "react-dom";
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Login/>
+       </div>
+       
+    );
+  }
+}
+
+export default App;
+
+
+
+
